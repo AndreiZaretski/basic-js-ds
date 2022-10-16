796b173337534aceab0fa18b74f75a2b513296a0
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2022-10-16 12:15:37 +0300

    feat: solution remove-from-list

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,13 +6,7 @@ const { Node } = require('../extensions/list-tree.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-// class Node {
-//   constructor(data) {
-//     this.data = data;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }  //Создаем класс 
+ 
 
 class BinarySearchTree {
 
